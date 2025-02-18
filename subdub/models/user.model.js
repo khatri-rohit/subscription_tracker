4763be3275be6677a575798c35d362c8c4cf0b9b
Rohit Khatri <111869337+khatri-rohit@users.noreply.github.com>
2025-02-18 13:16:59 +0000

    Database Models

---
+++ subdub/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'User Name is required'], // with error message
+        trim: true, // remove white spaces
+        minLength: 2,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'User Email is required'], // with error message
+        unique: true, // unipue email
+        trim: true, // remove white spaces
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], // email validation
+    },
+    password: {
+        type: String,
+        required: [true, 'User Password is required'], // with error message
+        minLength: 6,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
+// example : {name: 'John Doe' , email: 'johndoe@gmai.com', password: 'password123'}

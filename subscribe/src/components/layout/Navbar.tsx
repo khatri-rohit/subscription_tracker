8ad990512ccdcdcb1a210a94c3f68f4c202c69a8
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-03-03 02:13:26 +0530

    Refactor layout components: comment out Footer in Layout and enhance Navbar with structured markup and styling

--- subscribe/src/components/layout/Navbar.tsx
+++ subscribe/src/components/layout/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from "../ui/button"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <header>
+            <nav className="shadow-lg">
+                <div className="navbar flex items-center justify-between p-3">
+                    <div className="nav-left flex items-center justify-between">
+                        <img className="object-cover mx-1" src="/img/icon.png" alt="brand-icon" />
+                        <h1 className="text-4xl mx-1">SubScribe</h1>
+                    </div>
+                    <div className="nav-right ">
+                        <Button className="bg-[#636AE8]"> <img src="/img/newsletter.png" alt="" /> SignUp</Button>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar

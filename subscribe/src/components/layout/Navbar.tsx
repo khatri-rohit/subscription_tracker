607acb4beb774e55248790a509c3532ef0253443
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-03-17 00:44:11 +0530

    Add subscription overview and filtering to Dashboard; update Navbar and Home page layout

--- subscribe/src/components/layout/Navbar.tsx
+++ subscribe/src/components/layout/Navbar.tsx
@@ -1,16 +1,31 @@
-import { Button } from "../ui/button"
+import { NavLink } from "react-router-dom"
+// import { Button } from "../ui/button"
+import { Settings } from "lucide-react"
 
 const Navbar = () => {
     return (
         <header>
             <nav className="shadow-lg">
                 <div className="navbar flex items-center justify-between p-3">
-                    <div className="nav-left flex items-center justify-between">
-                        <img className="object-cover mx-1" src="/img/icon.png" alt="brand-icon" />
-                        <h1 className="text-4xl mx-1">SubScribe</h1>
+                    <div className="nav-left flex items-end justify-between">
+                        <div className="flex items-center">
+                            <img className="object-cover mx-1" src="/img/icon.png" alt="brand-icon" />
+                            <NavLink to={"/"}>
+                                <h1 className="text-4xl mx-1">SubScribe</h1>
+                            </NavLink>
+                        </div>
+                        <div className="navbar px-6">
+                            <NavLink to={"/home"} className={({ isActive }) => isActive ? "text-xl text-[#636AE8] font-medium relative after:absolute after:-bottom-[13px] after:w-[115%] after:-left-[6.5px] after:block after:h-[3px] after:rounded-b-4xl after:bg-[#636AE8]" : "text-xl"}>Dashboard</NavLink>
+                        </div>
                     </div>
-                    <div className="nav-right ">
-                        <Button className="bg-[#636AE8] hover:bg-[#565ba9] transition-all duration-300 cursor-pointer"> <img src="/img/newsletter.png" alt="" /> SignUp</Button>
+                    <div className="nav-right flex items-center justify-between gap-x-2">
+                        {/* <Button className="bg-[#636AE8] hover:bg-[#565ba9] transition-all duration-300 cursor-pointer"> <img src="/img/newsletter.png" alt="" /> SignUp</Button> */}
+
+                        <Settings />
+                        <div className="image">
+                            <img src="/img/blank-avatar.webp" alt="avatar" className="object-contain w-10 rounded-full" />
+                        </div>
+
                     </div>
                 </div>
             </nav>
--- subscribe/src/pages/Dashborad.tsx
+++ subscribe/src/pages/Dashborad.tsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import { Input } from "@/components/ui/input"
+import { List, Music, Podcast, Search, Video } from "lucide-react"
+import subscription from './subscription.ts';
 
 const Dashborad = () => {
+
   return (
-    <div>Dashborad</div>
+    <section className="p-10">
+      <div className="p-2 space-y-5 w-fit">
+        <h3 className="text-3xl">Subscription Overview</h3>
+        {/* Search Overview */}
+        <div className="flex items-center gap-x-2 w-full">
+          <div className="border-[.12em] border-[#BCC1CA] min-w-[75%] flex items-center rounded-lg shadow-2xs">
+            <Search className="mx-2" />
+            <Input className="border-none md:text-xl shadow-none"
+              placeholder="Search Subscription" />
+          </div>
+          <div className="tags text-[1em] flex items-center justify-between gap-x-2">
+            <span className="bg-gray-100 font-light px-2 rounded-2xl">
+              Active
+            </span>
+            <span className="bg-blue-100 text-blue-500 font-light px-2 rounded-2xl">
+              Pending
+            </span>
+            <span className="bg-red-100 text-red-500 font-light px-2 rounded-2xl">
+              Expired
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* All Subscriptions */}
+      <div className="grid grid-cols-2 md:gap-5 gap-2 lg:gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {
+          subscription.map((subs, _) => (
+            <div className="p-3 gap-3" key={_}>
+              <p className="text-xl font-medium">{subs.name}</p>
+              <p className="text-lg text-gray-500/80">{subs.status}</p>
+              <p>Renews: {subs.Renews}</p>
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="p-2 space-y-5 w-fit mt-8">
+        <h3 className="text-3xl">Filter & Search Subscriptions</h3>
+        {/* Search Overview */}
+        <div className="flex items-center gap-x-2">
+          <div className="border-[.12em] border-[#BCC1CA]  w-full flex items-center rounded-lg shadow-2xs">
+            <Search className="mx-2" />
+            <Input className="border-none md:text-xl shadow-none"
+              placeholder="Search Subscription" />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-evenly w-full gap-1">
+          <p className="text-lg flex justify-center items-center w-[25%] gap-x-1 relative hover:after:absolute hover:after:-bottom-[13px] hover:after:w-[115%] hover:after:-left-[6.5px] hover:after:block hover:after:h-[3px] hover:after:rounded-b-4xl hover:after:bg-[#636AE8] ">
+            <List color="#BBBBBB" />All
+          </p>
+          <p className="text-lg flex justify-center items-center w-[25%] gap-x-1 relative hover:after:absolute hover:after:-bottom-[13px] hover:after:w-[115%] hover:after:-left-[6.5px] hover:after:block hover:after:h-[3px] hover:after:rounded-b-4xl hover:after:bg-[#636AE8] ">
+            <Music color="#BBBBBB" />Music
+          </p>
+          <p className="text-lg flex justify-center items-center w-[25%] gap-x-1 relative hover:after:absolute hover:after:-bottom-[13px] hover:after:w-[115%] hover:after:-left-[6.5px] hover:after:block hover:after:h-[3px] hover:after:rounded-b-4xl hover:after:bg-[#636AE8] ">
+            <Video color="#BBBBBB" /> Video
+          </p>
+          <p className="text-lg flex justify-center items-center w-[25%] gap-x-1 relative hover:after:absolute hover:after:-bottom-[13px] hover:after:w-[115%] hover:after:-left-[6.5px] hover:after:block hover:after:h-[3px] hover:after:rounded-b-4xl hover:after:bg-[#636AE8] ">
+            <Podcast color="#BBBBBB" />Podcasts
+          </p>
+        </div>
+      </div>
+
+      {/* All Subscriptions */}
+      <div className="grid grid-cols-2 md:gap-5 gap-2 lg:gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {
+          subscription.map((subs, _) => (
+            <div className="p-3 gap-3" key={_}>
+              <p className="text-xl font-medium">{subs.name}</p>
+              <p className="text-lg text-gray-500/80">{subs.status}</p>
+              <p>Renews: {subs.Renews}</p>
+            </div>
+          ))
+        }
+      </div>
+
+
+
+    </section>
   )
 }
 

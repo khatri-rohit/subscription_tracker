146382894d8c210ec6391412d56f7c20424455c2
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-04-11 20:51:13 +0530

    Acject Debugging response

--- subdub/middleware/arcjet.middleware.js
+++ subdub/middleware/arcjet.middleware.js
@@ -7,15 +7,17 @@ const arcjetMiddleware = async (req, res, next) => {
 
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) {
+                console.log("Rate Limit Exceeded");
                 return res.status(429).json({ error: "Rate Limit Exceeded" });
             }
             if (decision.reason.isBot()) {
+                console.log("Bot Detected");
                 return res.status(403).json({ error: "Bot Detected" });
             }
-
+            
+            console.log("Access Deined");
             return res.status(403).json({ error: "Access Denied" });
         }
-        console.log(decision);
         next();
     } catch (error) {
         console.log(`Arcjet Middleware Error: ${error}`);
